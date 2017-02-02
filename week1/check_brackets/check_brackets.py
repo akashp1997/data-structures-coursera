142f48802a44531626613b131436f6100892c28a
Akash Purandare <akashp1997@Akashs-MacBook-Pro.local>
2017-02-03 01:03:02 +0530

    Final submission of the course. Inclusion of other assignments.

--- week1/check_brackets/check_brackets.py
+++ week1/check_brackets/check_brackets.py
@@ -1,5 +1,6 @@
-# python3
+# python2
 
+from os import listdir
 import sys
 
 class Bracket:
@@ -16,30 +17,39 @@ class Bracket:
             return True
         return False
 
-if __name__ == "__main__":
-    text = sys.stdin.read()
 
+def checker(text):
     opening_brackets_stack = []
     for i, next in enumerate(text, start=1):
         if next == '(' or next == '[' or next == '{':
             # Process opening bracket, write your code here
-            opening_brackets_stack.append((i, next))
+            opening_brackets_stack.append(Bracket(next, i))
 
         if next == ')' or next == ']' or next == '}':
             # Process closing bracket, write your code here
             if not opening_brackets_stack:
-                print i
-                quit()
-	    
-            top = stack.pop()
-            if top.match(next):
-                print i
-                quit()
+                return i
+            
+            top = opening_brackets_stack.pop()
+            if not top.Match(next):
+                return i
+            
     if opening_brackets_stack:
-        top = stack.pop()
-        print top.position
-        quit()
-    print "Success"
-    quit()
+        top = opening_brackets_stack.pop()
+        return top.position
+    
+    return "Success"
 
     # Printing answer, write your code here
+
+if __name__ == "__main__":
+    text = sys.stdin.read()
+    #text = "{"
+    print(checker(text))
+"""
+tests = listdir("/Users/akashp1997/Documents/data-structures/data-structures/week1/check_brackets/tests")
+for i in tests:
+    print tests
+    test_case = open(i, "r").readline().strip("\n")
+    print test_case
+"""
